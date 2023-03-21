9c120e90a37e475ea174a99a70b69b8e8fe3d23f
mikewazowski15 <mikeg.i@hotmail.com>
2023-03-21 14:45:47 +0700

    update

--- app.js
+++ app.js
@@ -98,6 +98,15 @@ app.get('/get-data3', (req, res) => {
   });
 });
 
+app.get('/get-user', (req, res) => {
+  const sql = `SELECT * FROM users,tasks WHERE users.id = tasks.id_Staff`;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log('Data retrieved successfully!');
+    res.json(result);
+  });
+});
+
 app.get('/edit-data', (req, res) => {
   const taskID = req.query.taskID;
   const sql = `SELECT * FROM tasks WHERE task_ID = ?`;
